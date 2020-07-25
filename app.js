0d5559c989fed7e39d39a6bc96fe774d9985216a
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-07-25 13:03:00 +0800

    Added multiple pages, learn more and save buttons

--- app.js
+++ app.js
@@ -3,86 +3,111 @@
 console.log("Loaded")
 
 let foundItems = document.querySelector(".found-items");
+let index = 0;
+let type;
+let savedBooks = [];
 
-async function searcher(query){
-    url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+$("#previous").hide();
+$("#next").hide();
 
+document.querySelector("#search-book").addEventListener("click", () => {
+    type = "book";
+    console.log(type)
+    searcher(document.querySelector("#query").value, index, type)
+});
+
+document.querySelector("#search-author").addEventListener("click", () => {
+    type = "author";
+    searcher(document.querySelector("#query").value, index, type)
+});
+
+
+document.querySelector("#previous").addEventListener("click", () => {
+    searcher(document.querySelector("#query").value, index-=10, type)
+    $(document).on('click', '#previous', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 300);
+    });
+});
+
+document.querySelector("#next").addEventListener("click", () => {
+    searcher(document.querySelector("#query").value, index+=10, type)
+    $(document).on('click', '#next', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 300);
+    });
+});
+
+
+async function searcher(query, startIndex, type){
+    console.log(type);
+    if (type == "book"){
+        url = `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}`;
+    } else if (type == "author"){
+        url = `https://www.googleapis.com/books/v1/volumes?q=+inauthor:${query}&startIndex=${startIndex}`;
+    }
     response = await fetch(url);
-    console.log(response);
     let data = await response.json();
     console.log(data);
-    data.items.forEach(book => {
-        console.log(book.volumeInfo.authors)
-        console.log(book.volumeInfo.title)
-        console.log(`${book.volumeInfo.title} was published in ${book.volumeInfo.publishedDate} by ${book.volumeInfo.publisher}. The book, written by ${book.volumeInfo.authors} has ${book.volumeInfo.pageCount} pages. It is considered to be a ${book.volumeInfo.categories} book.`)
-    })
+
+    document.querySelector("#item-count").innerText = `Viewing ${startIndex+1} - ${startIndex+11} of ${data.totalItems} items.`
 
     foundItems.innerHTML = "";
 
     data.items.forEach(book =>{
-        newBook = document.createElement("DIV")
-        newBook.classList.add("col", "col-12", "col-lg-6", "py-2")
-        newBook.innerHTML = `<div class="card h-100">
-                        <div class="row card-body">
-                            <div class="col-sm-8">
-                                <h5 class="card-title">${book.volumeInfo.title}</h5>
-                                <p class="card-text">${book.volumeInfo.authors}</p>
-                                <p class="card-text">${book.volumeInfo.title} was published in ${book.volumeInfo.publishedDate} by ${book.volumeInfo.publisher}. The book, written by ${book.volumeInfo.authors} has ${book.volumeInfo.pageCount} pages. It is considered to be a ${book.volumeInfo.categories} book.</p>
-                                <a href="#" class="btn btn-sm btn-primary">Learn More</a>
-                                <a href="#" class="btn btn-sm btn-primary">Save</a>
+        try{
+            if(book.volumeInfo.authors.length>1){
+                book.volumeInfo.authors = book.volumeInfo.authors.join(" and ");
+            }
+            if(book.volumeInfo.publishedDate.length>4){
+                book.volumeInfo.publishedDate = book.volumeInfo.publishedDate.toString().slice(0, 4)
+            }
+            if (book.volumeInfo.description.length > 300) {
+                book.volumeInfo.description = book.volumeInfo.description.slice(0, 300) + "...";
+                console.log(book.volumeInfo.description.length)
+                // book.volumeInfo.publishedDate = book.volumeInfo.publishedDate.toString().slice(0, 4)
+            }
+            
+
+            newBook = document.createElement("DIV")
+            newBook.classList.add("col", "col-12", "col-lg-6", "py-2")
+            newBook.innerHTML = `<div class="card h-100">
+                            <div class="row card-body">
+                                <div class="col-sm-8">
+                                    <h5 class="card-title">${book.volumeInfo.title}</h5>
+                                    <p class="card-text">${book.volumeInfo.authors}</p>
+                                    <p class="card-text">${book.volumeInfo.description}</p>
+                                    <a href="https://books.google.com/books?id=${book.id}" target="_blank" class="btn btn-sm btn-primary">Learn More</a>
+                                    <a href="#" class="btn btn-sm btn-primary save-book" id="${book.volumeInfo.industryIdentifiers[1].identifier}">Save</a>
+                                </div>
+                                <img class="col-sm-4" src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="sans" />
                             </div>
-                            <img class="col-sm-4" src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="sans" />
-                        </div>
-                    </div>`
-        foundItems.appendChild(newBook);
-        console.log("added!")
+                        </div>`
+            foundItems.appendChild(newBook);
+
+            if (!startIndex == 0) {$("#previous").show()} else { $("#previous").hide()};
+            $("#next").show();
+
+        } catch(e){
+            console.log(e)
+        }
+
+       
     })
-}
 
-document.querySelector("#search").addEventListener("click", () => {
-    searcher(document.querySelector("#query").value)
-});
-
-let exampleBook = {
-    "authors": "F. Scott Fitzgerald",
-    "title": "The Great Gatsby",
-    "publishedDate": "1930",
-    "pageCount": "165",
-    "publisher": "NY Publishing",
-    "categories": "Fiction"    
+    $(".save-book").click((e) => {
+        e.preventDefault();
+        console.log(e.target.id);
+        savedBooks.push(e.target.id);
+        console.log(savedBooks);
+    });
 }
 
 
-
-// let bookCard = `<div class="col col-12 col-lg-6 py-2">
-//                     <div class="card">
-//                         <div class="row card-body">
-//                             <div class="col-sm-9">
-//                                 <h5 class="card-title">${exampleBook.title}</h5>
-//                                 <p class="card-text">${exampleBook.authors}</p>
-//                                 <p class="card-text">fjdokmasodcasdiojc</p>
-//                                 <a href="#" class="btn btn-sm btn-primary">Learn More</a>
-//                                 <a href="#" class="btn btn-sm btn-primary">Save</a>
-//                             </div>
-//                             <img class="col-sm-3" src="Images/template.png" alt="sans" />
-//                         </div>
-//                     </div>
-//                 </div>`
-
-//let foundItems = document.querySelector(".found-items");
-// newBook = document.createElement("DIV")
-// newBook.classList.add("col", "col-12", "col-lg-6", "py-2")
-// newBook.innerHTML = `<div class="card">
-//                         <div class="row card-body">
-//                             <div class="col-sm-9">
-//                                 <h5 class="card-title">${exampleBook.title}</h5>
-//                                 <p class="card-text">${exampleBook.authors}</p>
-//                                 <p class="card-text">fjdokmasodcasdiojc</p>
-//                                 <a href="#" class="btn btn-sm btn-primary">Learn More</a>
-//                                 <a href="#" class="btn btn-sm btn-primary">Save</a>
-//                             </div>
-//                             <img class="col-sm-3" src="Images/template.png" alt="sans" />
-//                         </div>
-//                     </div>`
-// foundItems.appendChild(newBook);
-// console.log("added!")
+//<p class="card-text">${book.volumeInfo.title} was published in ${book.volumeInfo.publishedDate} by ${book.volumeInfo.publisher}. The book, written by ${book.volumeInfo.authors}, has ${book.volumeInfo.pageCount} pages. It is considered to be a ${book.volumeInfo.categories} book.</p>
