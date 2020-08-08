f5519bb9adb44b7e1b128edb6c1cbe8bb826f989
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-08-08 21:58:56 +0800

    Users can adjust their ratings and reviews. App now has CRUD features

--- public/saved.js
+++ public/saved.js
@@ -4,6 +4,8 @@ savedBooks = JSON.parse(localStorage.getItem('books'));
 console.log(savedBooks);
 
 linkActivation = () => {
+    console.log("activation started");
+
     $(".unsave-book").click((e) => {
         e.preventDefault();
         savedBooks = savedBooks.filter((item) => item.id !== `${e.target.id}`)
@@ -26,31 +28,51 @@ linkActivation = () => {
         currentlyEditingBook = "";
         currentlyEditingBook = e.target.id;
         currentlyEditingBook = currentlyEditingBook.slice(4)
+        e.preventDefault();
         console.log(currentlyEditingBook);
         $("#bookRating").val("");
         $("#bookReview").val("");
+        console.log(e);
+        currentEdit = e.target.parentElement.id.slice(2);
     })
 
     $(".saveRating").click((e) => {
-        e.preventDefault();
         console.log("saving rating");
-        editingBook = "";
+        //editingBook = "";
         rating = ""; review = "";
         console.log($("#bookRating").val())
         console.log($("#bookReview").val())
         rating = $("#bookRating").val();
         review = $("#bookReview").val();
 
+        ratingText = rating ? `You rated this book ${rating}/5.` : `You haven't rated this book yet.`
+        reviewText = review ? review : `You haven't reviewed this book yet.`
 
-        for(let i = 0; i < savedBooks.length; i++){
-            console.log(savedBooks[i].id);
-            if (savedBooks[i].id == currentlyEditingBook){
-                console.log("Found the book: " + savedBooks[i].id);
-                savedBooks[i].review = review;
-                savedBooks[i].rating = rating;
+        $(`#rating${currentEdit}`).html(ratingText);
+        $(`#review${currentEdit}`).html(reviewText);
+
+        // theChosenBook = savedBooks.filter(book => {
+        //     //console.log(book)
+        //     return book["id"] == currentlyEditingBook
+        // })
+        //console.log(theChosenBook);
+        // theChosenBook[0].review = $("#bookReview").val();
+        // theChosenBook[0].rating = $("#bookRating").val();
+        // console.log(theChosenBook);
+        // console.log(savedBooks);
+
+        savedBooks.forEach(book => {
+            //console.log(book.id);
+            if (book.id == currentlyEditingBook){
+                console.log("Found the book: " +book.id);
+                book.review = review;
+                book.rating = rating;
                 console.log(savedBooks);
+                writeToDB();
             }
-        }
+        })
+
+        //loadList();
 
         // editingBook = savedBooks.filter((item) => item.id == currentlyEditingBook);
         // savedBooks = savedBooks.filter((item) => item.id !== currentlyEditingBook);
@@ -63,40 +85,47 @@ linkActivation = () => {
 
 
         localStorage.setItem('books', JSON.stringify(savedBooks));
-        
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                db.collection("users").doc(`${user.displayName}`).set({
-                    books: JSON.stringify(savedBooks)
-                })
-            }
-        })
-
-        loadList();
+        function writeToDB(){
+            firebase.auth().onAuthStateChanged(function (user) {
+                if (user) {
+                    db.collection("users").doc(`${user.displayName}`).set({
+                        books: JSON.stringify(savedBooks)
+                    })
+                }
+            })
+            //setTimeout(loadList(), 5000);
+            //loadList();
+        };
+        //writeToDB();
+        //loadList();
     })
 }
 
 
 loadList = () => {
+    console.log("LL started");
     foundItems.innerHTML = "";
     document.querySelector("#item-count").innerText = `You've saved ${savedBooks.length} books.`
     
     $("#previous").hide(); $("#next").hide();
 
+    let counter = 0;
 
     savedBooks.forEach(book => {
-        console.log(book.rating);
+        ++counter;
+        //console.log(counter);
+        //console.log(book.rating);
         newBook = document.createElement("DIV")
         newBook.classList.add("col", "col-12", "col-md-6", "py-2")
         ratingText = book.rating ? `You rated this book ${book.rating}/5.` : `You haven't rated this book yet.`
         reviewText = book.review ? book.review : `You haven't reviewed this book yet.`
         newBook.innerHTML = `<div class="card h-100">
                                 <div class="row card-body">
-                                    <div class="col-8">
+                                    <div class="col-8" id="bk${counter}">
                                         <h5 class="card-title">${book.title}</h5>
                                         <p class="card-text">${book.author}</p>
-                                        <p class="rating">${ratingText}</p>
-                                        <p class="review"><strong>Your review:</strong><br/>${reviewText}</p>
+                                        <p class="rating" id="rating${counter}">${ratingText}</p>
+                                        <p class="review" id="review${counter}"><strong>Your review:</strong><br/>${reviewText}</p>
                                         <a href="${book.learnLink}" target="_blank" class="btn btn-sm btn-primary">Learn More</a>
                                         <a href="#" class="btn btn-sm btn-primary edit-book" id="edit${book.id}" data-toggle="modal" data-target="#ratingModal">Edit</a>
                                         <a href="#" class="btn btn-sm btn-primary unsave-book" id="${book.id}">Remove</a>
@@ -135,6 +164,8 @@ authCheck = () => {
                         localStorage.setItem(`books`, JSON.stringify(savedBooks));
                     }
                     loadList(); //Calling the function that loads the list on the page
+                    //linkActivation();
+
                 } else {
                     // doc.data() will be undefined in this case
                     console.log("No such document!");
